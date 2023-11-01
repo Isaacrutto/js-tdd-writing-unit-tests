2b92c8a99525203992997907d2991fdae8128563
Michael Friedman <minorpianokeys@gmail.com>
2023-11-01 11:33:04 -0400

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -6,7 +6,7 @@ import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
     it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
-        const word = "test";
+        const word = "tEst";
 
         const points = pointsForWord(word);
 
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
 export function pointsForWord(word) {
     let points = 0;
     for (const char of word) {
-        if (["a", "e", "i", "o", "u"].includes(char)) {
+        if (/["a", "e", "i", "o", "u"]/i.test(char)) {
             points += 1;
         } else {
             points += 2;
